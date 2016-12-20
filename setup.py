8d1c81354b986c175b9f5a51587c40c8deb9ddbf
伽楼 <jialou.jp@alibaba-inc.com>
2016-12-20 17:15:41 +0800

    add setup for kaffe

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='kaffe',
+      version='1.0',
+      description='Caffe And Tensorflow Network Builder and Transformer',
+      author='jpbirdy',
+      author_email='jiangpeng285@qq.com',
+      packages=['kaffe', 'kaffe.caffe', 'kaffe.tensorflow'],
+      )
